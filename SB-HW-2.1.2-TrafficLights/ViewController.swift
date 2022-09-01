f77403e5a82971055e66e472026c2c4ac4f9887f
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-09-01 23:19:41 +0300

    Initial Commit

---
+++ SB-HW-2.1.2-TrafficLights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SB-HW-2.1.2-TrafficLights
+//
+//  Created by Sergey Nestroyniy on 01.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
