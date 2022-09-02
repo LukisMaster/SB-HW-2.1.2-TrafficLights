dd90246c23e5622bcc8826f8511a37c952c1ae83
LukisMaster <112563277+LukisMaster@users.noreply.github.com>
2022-09-02 23:39:14 +0300

    Initial commit - заверстал, симулятор отображает

--- SB-HW-2.1.2-TrafficLights/ViewController.swift
+++ SB-HW-2.1.2-TrafficLights/ViewController.swift
@@ -8,12 +8,47 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+//
+//
+//    @IBOutlet var buttonStartNext: UIButton!
+//    @IBOutlet var redView: UIView!
+//    @IBOutlet var yellowView: UIView!
+//    @IBOutlet var greenView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    enum Lights {
+        case Start
+        case Red
+        case Yellow
+        case Green
+    }
+    
+//    @IBAction func pressButton(_ sender: Any) {
+//        var stage = Lights.Start
+//        switch stage {
+//        case .Start:
+//            buttonStartNext.setTitle("NEXT", for: buttonStartNext.state)
+//            redView.alpha = 1
+//            stage = .Red
+//        case .Red:
+//            redView.alpha = 0.3
+//            yellowView.alpha = 1
+//            stage = .Yellow
+//        case .Yellow:
+//            yellowView.alpha = 0.3
+//            greenView.alpha = 1
+//            stage = .Green
+//        case .Green:
+//            greenView.alpha = 0.3
+//            redView.alpha = 1
+//            stage = .Red
+//        }
+//        
+//    }
+    
 }
 
